f7bc695fbb77a09651914bf604950e4623b309e1
Carter Venne <you@example.com>
2021-05-13 21:12:31 -0500

    Removed nav/footer form pages, start App construct

--- gonefishin/src/pages/Home.js
+++ gonefishin/src/pages/Home.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import Map from "../components/Map";
 import SearchContainer from "../components/SearchContainer";
 
--- gonefishin/src/pages/Login.js
+++ gonefishin/src/pages/Login.js
@@ -1,8 +1,21 @@
 import React from "react";
 import LoginCard from "../components/LoginCard";
 import AccountCreationCard from "../components/AccountCreationCard";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
+function Login () {
+    return (
+        <div>
+            {/* these classNames are bootstrap components not sure what to change */}
+            <h1 className="text-center">Welcome to Gone Fishin'</h1>
+            <h3 className="text-center">Please log in or create your account and get fishing</h3>
+            <LoginCard>
+
+            </LoginCard>
+            <AccountCreationCard>
+
+            </AccountCreationCard>
+        </div>
+    );
+}
 
 export default Login;
