deee7cbc5ec91218249c2f3837066ae077277089
Carter Venne <you@example.com>
2021-05-20 20:46:36 -0500

    looks pretty,bout it for now

--- gonefishin/src/components/LoginHeader.js
+++ gonefishin/src/components/LoginHeader.js
@@ -3,10 +3,10 @@ import '../styles/main.css';
 
 function LoginHeader () {
     return (
-    <div className="h-auto bg-gray flex flex-col space-y-10 object-center items-center pb-5">
-        <div className="bg-gray w-96 shadow-xl rounded p-5 pb-5">
-            <h1 className="text-3xl font-medium text-center">Welcome</h1>
-            <p className="text-sm text-center">If you already have an account please log in, otherwise take the time to create one below</p>
+    <div className="h-auto bg-gray flex flex-col space-y-10 object-center items-center pt-10 pb-5">
+        <div className="bg-yellow-800 bg-opacity-50 w-96 shadow-xl rounded p-5 pb-5">
+            <h1 className="text-3xl font-medium text-white text-center">Welcome</h1>
+            <p className="text-md text-white text-center">If you already have an account please log in, otherwise take the time to create one below</p>
         </div>
     </div>
     )
